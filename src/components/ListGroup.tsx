feb4fc1e9891b4479e5fb66a34b6336ebd0553d0
Yanica Zafirova <yanica.zafirova.dev@gmail.com>
2025-01-23 10:56:14 +0200

    Test commit.

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -2,7 +2,13 @@
 
 function ListGroup(){
     return (
-        <div>test</div>
+        <ul className="list-group">
+            <li className="list-group-item">An item</li>
+            <li className="list-group-item">A second item</li>
+            <li className="list-group-item">A third item</li>
+            <li className="list-group-item">A fourth item</li>
+            <li className="list-group-item">And a fifth one</li>
+        </ul>
     );
 }
 
